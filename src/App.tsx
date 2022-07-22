4938000cf737e2abdd851a810c017421e34b3ce5
david.csizy <david.csizy@gmail.com>
2022-07-22 16:13:03 +0200

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//  import logo from './logo.svg';
+import mainStyles from './styles/App.module.css';
+import bgStyles from './styles/background-rectange.module.css'
+
+import NavigationBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={bgStyles.rectangle}>
+      <NavigationBar/>
+      <div className={mainStyles.mainContent}>
+        <h3>HELLO, I&apos;M</h3>
+        <h2>DAVID CSIZY</h2>
+        <h4>Lorem ipsum dolor sit amet, consectetuer adip</h4>
+      </div>
     </div>
+
   );
 }
 
