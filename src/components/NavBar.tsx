3cf1fe621387b1f593b707b374bfea7b1a80b5b9
david.csizy <david.csizy@gmail.com>
2022-09-26 21:03:16 +0200

    Adding responsive navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from '../Styles/NavBar.module.css';
 import '../Styles/icofont.min.css';
 import initialsLogo from '../Images/initials.png';
@@ -9,41 +9,63 @@ const NavigationBar = () => {
     color: '#e7ff7f',
     textDecoration: 'none',
   };
+  const [isNavExpanded, setIsNavExpanded] = useState<boolean>(false);
 
+  const toggleButtonClick = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
   return (
-    <nav className={styles.navBox}>
-      <div id={styles.first}>
-        <NavLink to='/'>
-          <img id={styles.logo} src={initialsLogo} alt='My logo' />
-        </NavLink>
-      </div>
-      <button className={styles.hamburger}>
-        <i className='icofont-navigation-menu'></i>
-      </button>
-      <div id={styles.second}>
-        <NavLink to='/' style={({ isActive }) => (isActive ? activeStyle : {})}>
-          Home
-        </NavLink>
-        <NavLink
-          to='/about'
-          style={({ isActive }) => (isActive ? activeStyle : {})}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to='/skillsandtechs'
-          style={({ isActive }) => (isActive ? activeStyle : {})}
-        >
-          <span>Techs</span>
-        </NavLink>
-        <NavLink
-          to='/contactme'
-          style={({ isActive }) => (isActive ? activeStyle : {})}
-        >
-          <span>Contact</span>
-        </NavLink>
-      </div>
-    </nav>
+    <>
+      <nav
+        className={isNavExpanded ? styles.openNavMenu : styles.closedNavMenu}
+      >
+        <div id={styles.first}>
+          <NavLink to='/'>
+            <img id={styles.logo} src={initialsLogo} alt='My logo' />
+          </NavLink>
+        </div>
+        <div id={styles.second}>
+          <NavLink
+            to='/'
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            onClick={() => toggleButtonClick()}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/about'
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            onClick={() => toggleButtonClick()}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to='/skillsandtechs'
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            onClick={() => toggleButtonClick()}
+          >
+            <span>Techs</span>
+          </NavLink>
+          <NavLink
+            to='/contactme'
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            onClick={() => toggleButtonClick()}
+          >
+            <span>Contact</span>
+          </NavLink>
+          <button
+            className={styles.hamburger}
+            onClick={() => toggleButtonClick()}
+          >
+            <i
+              className={
+                isNavExpanded ? 'icofont-ui-close' : 'icofont-navigation-menu'
+              }
+            ></i>
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
