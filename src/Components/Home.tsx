b8c7ab4bda912788507687f609ae535f75300cc9
david.csizy <david.csizy@gmail.com>
2022-10-02 21:51:06 +0200

    Changes to title and navbar

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,49 +2,40 @@ import React from 'react';
 import styles from '../Styles/Home.module.css';
 import '../Styles/icofont.min.css';
 import '../Styles/brands.min.css';
-/* import circuit from '../circuit.svg';
-import HomeCircuitSvgComponent from './HomeCircuitSvg'; */
 
 const HomeComponent = () => {
   return (
-    <>
-      <section className={styles.mainContent}>
-        {/*         <div className={styles.imageContainer}>
-          <HomeCircuitSvgComponent />
-        </div> */}
-        <div className={styles.textContainer}>
-          <h3 className={styles.welcome}>HELLO, I&apos;M</h3>
-          <h2 className={styles.name}>DAVID CSIZY</h2>
-          <h4 className={styles.summary}>
-            Experienced supply chain analyst
-            <br />
-            with enthusiasm for digital and automated solutions.
-          </h4>
-          <section className={styles.social}>
-            <span>
-              <a
-                href='https://www.linkedin.com/in/david-csizy-7ab5a063/'
-                target='_blank'
-                rel='noreferrer'
-                className='icofont-linkedin'
-              ></a>
-              <a
-                href='http://m.me/david.csizy'
-                target='_blank'
-                rel='noreferrer'
-                className='icofont-facebook-messenger'
-              ></a>
-              <a
-                href='https://github.com/dvdcszy'
-                target='_blank'
-                rel='noreferrer'
-                className='fa-brands fa-github'
-              ></a>
-            </span>
-          </section>
-        </div>
+    <div className='container is-align-items-center is-justify-content-center'>
+      <h3 className='title is-3 is-size-5-touch'>HELLO, I&apos;M</h3>
+      <p className='title is-1 is-size-2-touch'>DAVID CSIZY</p>
+      <h4 className='subtitle is-3 is-size-5-touch'>
+        Experienced supply chain analyst
+        <br />
+        with enthusiasm for digital and automated solutions.
+      </h4>
+      <section className={styles.social}>
+        <span>
+          <a
+            href='https://www.linkedin.com/in/david-csizy-7ab5a063/'
+            target='_blank'
+            rel='noreferrer'
+            className='icofont-linkedin'
+          ></a>
+          <a
+            href='http://m.me/david.csizy'
+            target='_blank'
+            rel='noreferrer'
+            className='icofont-facebook-messenger'
+          ></a>
+          <a
+            href='https://github.com/dvdcszy'
+            target='_blank'
+            rel='noreferrer'
+            className='fa-brands fa-github'
+          ></a>
+        </span>
       </section>
-    </>
+    </div>
   );
 };
 
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -10,6 +10,10 @@ const NavigationBar = () => {
   const toggleBurger = () => {
     navRef.current.classList.toggle('is-active');
   };
+  const closeNavmenu = () => {
+    const hasClassIsActive = navRef.current.classList.contains('is-active');
+    if (hasClassIsActive) navRef.current.classList.remove('is-active');
+  };
   return (
     <nav className='navbar' role='navigation' aria-label='main navigation'>
       <div className='navbar-brand'>
@@ -29,10 +33,18 @@ const NavigationBar = () => {
       <div ref={navRef} className='navbar-menu' id='nav-links'>
         <div className='navbar-end'>
           <a className='navbar-item'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/skillsandtechs'>Skills</NavLink>
-            <NavLink to='/contactme'>Contact</NavLink>
+            <NavLink to='/' onClick={() => closeNavmenu()}>
+              Home
+            </NavLink>
+            <NavLink to='/about' onClick={() => closeNavmenu()}>
+              About
+            </NavLink>
+            <NavLink to='/skillsandtechs' onClick={() => closeNavmenu()}>
+              Skills
+            </NavLink>
+            <NavLink to='/contactme' onClick={() => closeNavmenu()}>
+              Contact me
+            </NavLink>
           </a>
         </div>
       </div>
